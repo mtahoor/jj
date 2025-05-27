24e0c339004537f8b9aedd35ee15cd36dd7e35eb
mtahoor <mtahoorali@gmail.com>
2025-05-27 13:28:20 +0500

    add procfile

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,10 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-default-key-for-developmen
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+
+ALLOWED_HOSTS = ['*']
+
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 # Application definition
 INSTALLED_APPS = [
